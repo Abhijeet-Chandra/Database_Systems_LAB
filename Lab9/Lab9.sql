e691af71ba1343e40d83f4bbf679ea4c955eacbb
MAXRIZZ_ABHIJEET <86531959+Abhijeet-Chandra@users.noreply.github.com>
2026-03-22 23:47:53 +0530

    Implement function to find highest paid instructor
    Added a PL/SQL function to retrieve the highest paid instructor for each department and a block to display the results.

--- Lab9/Lab9.sql
+++ Lab9/Lab9.sql
@@ -120,3 +120,31 @@ SQL> BEGIN
   3  END;
   4  /
 
+-- Q6)Based on the University Database Schema in Lab 2, write a Pl/Sql block of code that lists the highest paid Instructor in each of the Department. It should make use of a function department_highest which returns the highest paid Instructor for the given branch.
+
+SQL> CREATE OR REPLACE FUNCTION display_q6(p_dept_name VARCHAR2)
+  2  return VARCHAR2 as
+  3  highest instructor.name%type;
+  4  BEGIN
+  5     SELECT name INTO highest
+  6     FROM instructor
+  7     WHERE dept_name = p_dept_name
+  8     AND salary = (
+  9             SELECT MAX(salary) FROM instructor
+ 10             WHERE dept_name = p_dept_name
+ 11     );
+ 12
+ 13     RETURN highest;
+ 14  END;
+ 15  /
+
+Function created.
+
+SQL>
+SQL> BEGIN
+  2     FOR x IN (select DISTINCT dept_name FROM instructor)
+  3     LOOP
+  4             DBMS_OUTPUT.PUT_LINE(x.dept_name || ' -> ' || display_q6(x.dept_name));
+  5     END LOOP;
+  6  END;
+  7  /
